0bf8ed9b545a1e53842cf8c75b7388720e2f8f7c
ConCode <concode@outlook.de>
2021-11-12 19:51:00 +0100

    Relocated some Stuff

--- src/TrinkgeldCalculator/TrinkgeldPage.tsx
+++ src/TrinkgeldCalculator/TrinkgeldPage.tsx
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Trinkgeld.css";
+import { Helmet } from "react-helmet";
 
 function TrinkgeldPage() {
   const [persons, setPersons] = useState<{ name: string; hours: number }[]>([]);
@@ -56,116 +58,160 @@ function TrinkgeldPage() {
 
   if (calculationPhase === 1)
     return (
-      <div className="App">
-        <h2>Trinkgeld Rechner</h2>
-        <p>
-          Es wurden wurden{" "}
-          <b>
-            <input
-              type={`number`}
-              className={`hidden`}
-              style={{ width: `${`${money}`.length + 0.5}ch` }}
-              onChange={(e) => {
-                if (e.target.value === ``) return;
-                setMoney(parseFloat(e.target.value));
-                e.target.style.width = `${`${parseFloat(e.target.value)}`.length + 0.5}ch`;
-              }}
-              defaultValue={money}
-            ></input>{" "}
-            €{" "}
-          </b>{" "}
-          an Trinkgeld eingenommen.
-          <br />
-          Es wurden insgesamt <b>{calculateHours(persons)} Stunden </b> gearbeitet.
-        </p>
-        <div>
-          <h4>Person Hinzufügen</h4>
-          <input ref={nameInput} placeholder={`Name`} />{" "}
-          <input type={"number"} ref={hoursInput} placeholder={`Hours`} />{" "}
-          <button onClick={addPerson}>Add</button>
-        </div>
-        <div className={`content`}>
-          {persons.map((p) => {
-            return (
-              <>
-                <button
-                  onClick={() => {
-                    removePerson(p.name);
-                  }}
-                >
-                  X
-                </button>{" "}
-                {p.name} - <b>{p.hours} Stunden</b>
-                <br />
-              </>
-            );
-          })}
-        </div>
-        <button
-          onClick={() => {
-            if (persons.length > 0) {
-              setCalcPhase(2);
-            }
-          }}
-          className={`BigButton`}
-        >
-          Finish Calculation
-        </button>
-      </div>
-    );
-  else if (calculationPhase === 2)
-    return (
-      <div className="App">
-        <h2>
-          {getPerson().name}
-          <p></p>
-        </h2>
-        <div className={`content`}>
-          <h3>
-            Trinkgeld: <b>{(money * getPerson().percent).toFixed(2)}€</b>
-          </h3>
+      <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Trinkgeld Rechner</title>
+      </Helmet>
+        <nav>
+          <ol>
+            <li>
+              <Link to={`/`}>Menu</Link>
+            </li>
+            <li>Trinkgeld Rechner</li>
+          </ol>
+        </nav>
+        <div className="App">
+          <h2>Trinkgeld Rechner</h2>
           <p>
-            Stunden gearbeitet: <b>{persons[currPersonIndex].hours}h</b>
+            Es wurden wurden{" "}
+            <b>
+              <input
+                type={`number`}
+                className={`hidden`}
+                style={{ width: `${`${money}`.length + 0.5}ch` }}
+                onChange={(e) => {
+                  if (e.target.value === ``) return;
+                  setMoney(parseFloat(e.target.value));
+                  e.target.style.width = `${`${parseFloat(e.target.value)}`.length + 0.5}ch`;
+                }}
+                defaultValue={money}
+              ></input>{" "}
+              €{" "}
+            </b>{" "}
+            an Trinkgeld eingenommen.
             <br />
-            Prozentualer Anspruch: <b>{(getPerson().percent * 100).toFixed(2)} %</b>
+            Es wurden insgesamt <b>{calculateHours(persons)} Stunden </b> gearbeitet.
           </p>
-        </div>
-        <div className={`buttons`}>
-          <button onClick={prevPerson} className={`BigButton`}>
-            {(currPersonIndex === 0 && <>Back to Menu</>) || <>Previous Person</>}
-          </button>
-          {currPersonIndex < persons.length - 1 && (
-            <button onClick={nextPerson} className={`BigButton`}>
-              Next Person
+          <div>
+            <h4>Person Hinzufügen</h4>
+            <input ref={nameInput} placeholder={`Name`} />{" "}
+            <input type={"number"} ref={hoursInput} placeholder={`Hours`} />{" "}
+            <button onClick={addPerson}>Add</button>
+          </div>
+          <div className={`content`}>
+            {persons.map((p) => {
+              return (
+                <>
+                  <button
+                    onClick={() => {
+                      removePerson(p.name);
+                    }}
+                  >
+                    X
+                  </button>{" "}
+                  {p.name} - <b>{p.hours} Stunden</b>
+                  <br />
+                </>
+              );
+            })}
+          </div>
+          {persons.length > 0 && (
+            <button
+              onClick={() => {
+                if (persons.length > 0) {
+                  setCalcPhase(2);
+                }
+              }}
+              className={`BigButton`}
+            >
+              Fortfahren
             </button>
           )}
         </div>
-      </div>
+      </>
+    );
+  else if (calculationPhase === 2)
+    return (
+      <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{getPerson().name} - Trinkgeld Rechner</title>
+      </Helmet>
+        <nav>
+          <ol>
+            <li>
+              <Link to={`/`}>Menu</Link>
+            </li>
+            <li>Trinkgeld Rechner</li>
+          </ol>
+        </nav>
+        <div className="App">
+          <h2>
+            {getPerson().name}
+            <p></p>
+          </h2>
+          <div className={`content`}>
+            <h3>
+              Trinkgeld: <b>{(money * getPerson().percent).toFixed(2)}€</b>
+            </h3>
+            <p>
+              Stunden gearbeitet: <b>{persons[currPersonIndex].hours}h</b>
+              <br />
+              Prozentualer Anspruch: <b>{(getPerson().percent * 100).toFixed(2)} %</b>
+            </p>
+          </div>
+          <div className={`buttons`}>
+            <button onClick={prevPerson} className={`BigButton`}>
+              {(currPersonIndex === 0 && <>Back to Menu</>) || <>Previous Person</>}
+            </button>
+            {currPersonIndex < persons.length - 1 && (
+              <button onClick={nextPerson} className={`BigButton`}>
+                Next Person
+              </button>
+            )}
+          </div>
+        </div>
+      </>
     );
   else {
     return (
-      <div className="App">
-        <h2>
-          Wie viel Trinkgeld wurde eingenommen?
-          <p></p>
-        </h2>
-        <div className={`content`}>
-          <input type={"number"} ref={moneyInput} placeholder={`0€`} />
+      <>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Trinkgeld Rechner</title>
+        </Helmet>
+        <nav>
+          <ol>
+            <li>
+              <Link to={`/`}>Menu</Link>
+            </li>
+            <li>Trinkgeld Rechner</li>
+          </ol>
+        </nav>
+        <div className="App">
+          <h2>
+            Wie viel Trinkgeld wurde eingenommen?
+            <p></p>
+          </h2>
+          <div className={`content`}>
+            <input type={"number"} ref={moneyInput} placeholder={`0€`} />
+          </div>
+          <div className={`buttons`}>
+            <button
+              onClick={() => {
+                if (moneyInput.current?.value !== ``) {
+                  setCalcPhase(1);
+                  setMoney(parseFloat(moneyInput.current?.value || `0`));
+                }
+              }}
+              className={`BigButton`}
+            >
+              Fortfahren
+            </button>
+          </div>
         </div>
-        <div className={`buttons`}>
-          <button
-            onClick={() => {
-              if (moneyInput.current?.value !== ``) {
-                setCalcPhase(1);
-                setMoney(parseFloat(moneyInput.current?.value || `0`));
-              }
-            }}
-            className={`BigButton`}
-          >
-            Fortfahren
-          </button>
-        </div>
-      </div>
+      </>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
+import TrinkgeldPage from "./TrinkgeldCalculator/TrinkgeldPage";
+import { Helmet } from "react-helmet";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route caseSensitive path={`/trinkgeld`} element={<TrinkgeldPage />} />
+        <Route caseSensitive path={`/`} element={<Menu />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+function Menu() {
+  return (
+    <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>FP Utilities</title>
+    </Helmet>
+      <nav>
+        <ol>
+          <li>
+            Menu
+          </li>
+        </ol>
+      </nav>
+      <div className="App">
+        <h2>FP Utils</h2>
+        <p></p>
+        <Link to={`/trinkgeld`}><button>Trinkgeld Rechner</button></Link>
+      </div>
+    </>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
