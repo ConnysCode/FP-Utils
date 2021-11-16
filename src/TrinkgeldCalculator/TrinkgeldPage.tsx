f795126bf0e282fa521d81b2987dacc71b6968ed
conny <36863945+ConnysCode@users.noreply.github.com>
2021-11-16 16:41:36 +0100

    Actual Conditional Rendering

--- src/TrinkgeldCalculator/TrinkgeldPage.tsx
+++ src/TrinkgeldCalculator/TrinkgeldPage.tsx
@@ -2,12 +2,14 @@ import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Trinkgeld.css";
 import { Helmet } from "react-helmet";
+import Person from "./components/person";
 
 function TrinkgeldPage() {
   const [persons, setPersons] = useState<{ name: string; hours: number }[]>([]);
   const [currPersonIndex, setCurrPersonIndex] = useState(0);
   const [calculationPhase, setCalcPhase] = useState(0);
   const [money, setMoney] = useState(0);
+  const [title, setTitle] = useState(`Trinkgeld Rechner`);
 
   const nameInput = useRef<HTMLInputElement>(null);
   const hoursInput = useRef<HTMLInputElement>(null);
@@ -23,7 +25,9 @@ function TrinkgeldPage() {
     if (person > -1) return;
     newP.push({ name: name, hours: hours });
     setPersons(newP);
-    nameInput.current?.focus();
+    console.log(nameInput.current);
+    
+    //nameInput.current?.focus();
     nameInput.current?.select();
   };
   const removePerson = (name: string) => {
@@ -56,164 +60,138 @@ function TrinkgeldPage() {
     };
   };
 
-  if (calculationPhase === 1)
+  const MoneyInit = () => {
     return (
       <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Trinkgeld Rechner</title>
-      </Helmet>
-        <nav>
-          <ol>
-            <li>
-              <Link to={`/`}>Menu</Link>
-            </li>
-            <li>Trinkgeld Rechner</li>
-          </ol>
-        </nav>
-        <div className="App">
-          <h2>Trinkgeld Rechner</h2>
-          <p>
-            Es wurden wurden{" "}
-            <b>
-              <input
-                type={`number`}
-                className={`hidden`}
-                style={{ width: `${`${money}`.length + 0.5}ch` }}
-                onChange={(e) => {
-                  if (e.target.value === ``) return;
-                  setMoney(parseFloat(e.target.value));
-                  e.target.style.width = `${`${parseFloat(e.target.value)}`.length + 0.5}ch`;
-                }}
-                defaultValue={money}
-              ></input>{" "}
-              €{" "}
-            </b>{" "}
-            an Trinkgeld eingenommen.
-            <br />
-            Es wurden insgesamt <b>{calculateHours(persons)} Stunden </b> gearbeitet.
-          </p>
-          <div>
-            <h4>Person Hinzufügen</h4>
-            <input ref={nameInput} placeholder={`Name`} />{" "}
-            <input type={"number"} ref={hoursInput} placeholder={`Hours`} />{" "}
-            <button onClick={addPerson}>Add</button>
-          </div>
-          <div className={`content`}>
-            {persons.map((p) => {
-              return (
-                <>
-                  <button
-                    onClick={() => {
-                      removePerson(p.name);
-                    }}
-                  >
-                    X
-                  </button>{" "}
-                  {p.name} - <b>{p.hours} Stunden</b>
-                  <br />
-                </>
-              );
-            })}
-          </div>
-          {persons.length > 0 && (
-            <button
-              onClick={() => {
-                if (persons.length > 0) {
-                  setCalcPhase(2);
-                }
+        <h2>
+          Wie viel Trinkgeld wurde eingenommen?
+          <p></p>
+        </h2>
+        <div className={`content`}>
+          <input type={"number"} ref={moneyInput} placeholder={`0€`} />
+        </div>
+        <div className={`buttons`}>
+          <button
+            onClick={() => {
+              if (moneyInput.current?.value !== ``) {
+                setCalcPhase(1);
+                setMoney(parseFloat(moneyInput.current?.value || `0`));
+              }
+            }}
+            className={`BigButton`}
+          >
+            Fortfahren
+          </button>
+        </div>
+      </>
+    );
+  };
+
+  const MainMenu = () => {
+    return (
+      <>
+        <h2>Trinkgeld Rechner</h2>
+        <p>
+          Es wurden wurden{" "}
+          <b>
+            <input
+              type={`number`}
+              className={`hidden`}
+              style={{ width: `${`${money}`.length + 0.5}ch` }}
+              onChange={(e) => {
+                if (e.target.value === ``) return;
+                setMoney(parseFloat(e.target.value));
+                e.target.style.width = `${`${parseFloat(e.target.value)}`.length + 0.5}ch`;
               }}
-              className={`BigButton`}
-            >
-              Fortfahren
+              defaultValue={money}
+            ></input>{" "}
+            €{" "}
+          </b>{" "}
+          an Trinkgeld eingenommen.
+          <br />
+          Es wurden insgesamt <b>{calculateHours(persons)} Stunden </b> gearbeitet.
+        </p>
+        <div>
+          <h4>Person Hinzufügen</h4>
+          <input ref={nameInput} placeholder={`Name`} />{" "}
+          <input type={"number"} ref={hoursInput} placeholder={`Hours`} />{" "}
+          <button onClick={addPerson}>Add</button>
+        </div>
+        <div className={`content`}>
+          {persons.map((p) => {
+            return (
+              <>
+                <button
+                  onClick={() => {
+                    removePerson(p.name);
+                  }}
+                >
+                  X
+                </button>{" "}
+                {p.name} - <b>{p.hours} Stunden</b>
+                <br />
+              </>
+            );
+          })}
+        </div>
+        {persons.length > 0 && (
+          <button
+            onClick={() => {
+              if (persons.length > 0) {
+                setCalcPhase(2);
+              }
+            }}
+            className={`BigButton`}
+          >
+            Fortfahren
+          </button>
+        )}
+      </>
+    );
+  };
+
+  const CalculateMenu = () => {
+    return (
+      <>
+        <h2>
+          {getPerson().name}
+          <p></p>
+        </h2>
+        <div className={`content`}>
+          <h3>
+            Trinkgeld: <b>{(money * getPerson().percent).toFixed(2)}€</b>
+          </h3>
+          <p>
+            Stunden gearbeitet: <b>{persons[currPersonIndex].hours}h</b>
+            <br />
+            Prozentualer Anspruch: <b>{(getPerson().percent * 100).toFixed(2)} %</b>
+          </p>
+        </div>
+        <div className={`buttons`}>
+          <button onClick={prevPerson} className={`BigButton`}>
+            {(currPersonIndex === 0 && <>Back to Menu</>) || <>Previous Person</>}
+          </button>
+          {currPersonIndex < persons.length - 1 && (
+            <button onClick={nextPerson} className={`BigButton`}>
+              Next Person
             </button>
           )}
         </div>
       </>
     );
-  else if (calculationPhase === 2)
-    return (
-      <>
+  };
+
+  return (
+    <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{getPerson().name} - Trinkgeld Rechner</title>
+        <title>{(calculationPhase === 2 && getPerson().name) || `Trinkgeld Rechner`}</title>
       </Helmet>
-        <nav>
-          <ol>
-            <li>
-              <Link to={`/`}>Menu</Link>
-            </li>
-            <li>Trinkgeld Rechner</li>
-          </ol>
-        </nav>
-        <div className="App">
-          <h2>
-            {getPerson().name}
-            <p></p>
-          </h2>
-          <div className={`content`}>
-            <h3>
-              Trinkgeld: <b>{(money * getPerson().percent).toFixed(2)}€</b>
-            </h3>
-            <p>
-              Stunden gearbeitet: <b>{persons[currPersonIndex].hours}h</b>
-              <br />
-              Prozentualer Anspruch: <b>{(getPerson().percent * 100).toFixed(2)} %</b>
-            </p>
-          </div>
-          <div className={`buttons`}>
-            <button onClick={prevPerson} className={`BigButton`}>
-              {(currPersonIndex === 0 && <>Back to Menu</>) || <>Previous Person</>}
-            </button>
-            {currPersonIndex < persons.length - 1 && (
-              <button onClick={nextPerson} className={`BigButton`}>
-                Next Person
-              </button>
-            )}
-          </div>
-        </div>
-      </>
-    );
-  else {
-    return (
-      <>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Trinkgeld Rechner</title>
-        </Helmet>
-        <nav>
-          <ol>
-            <li>
-              <Link to={`/`}>Menu</Link>
-            </li>
-            <li>Trinkgeld Rechner</li>
-          </ol>
-        </nav>
-        <div className="App">
-          <h2>
-            Wie viel Trinkgeld wurde eingenommen?
-            <p></p>
-          </h2>
-          <div className={`content`}>
-            <input type={"number"} ref={moneyInput} placeholder={`0€`} />
-          </div>
-          <div className={`buttons`}>
-            <button
-              onClick={() => {
-                if (moneyInput.current?.value !== ``) {
-                  setCalcPhase(1);
-                  setMoney(parseFloat(moneyInput.current?.value || `0`));
-                }
-              }}
-              className={`BigButton`}
-            >
-              Fortfahren
-            </button>
-          </div>
-        </div>
-      </>
-    );
-  }
+      <div className="App">
+        {(calculationPhase === 2 && <CalculateMenu />) ||
+          (calculationPhase === 1 && <MainMenu />) || <MoneyInit />}
+      </div>
+    </>
+  );
 }
-
 export default TrinkgeldPage;
