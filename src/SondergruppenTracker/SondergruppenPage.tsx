f64478886bd7514e2fe7392b2a00642aadefdd7c
conny <36863945+ConnysCode@users.noreply.github.com>
2022-05-28 01:41:00 +0200

    worked on sondergruppen tracker
    Probably canceling this project (sondergruppen tracker)

--- src/SondergruppenTracker/SondergruppenPage.tsx
+++ src/SondergruppenTracker/SondergruppenPage.tsx
@@ -2,14 +2,20 @@ import React, { useEffect, useRef, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 // import "./sondergruppen.css";
 import { Helmet } from "react-helmet";
-import { GeburtstagLaserTag, groups, groupsType, softdrinks } from "./sondergruppenInterfaces";
 import "../index.css";
 import "./sondergruppen.css";
 import { isTemplateTail } from "typescript";
+import {
+  bookingTypeInterface,
+  bookingTypes,
+  softdrinks,
+  sondergruppeInterface,
+} from "./sondergruppenInterfaces";
 
 function SondergruppenPage() {
-  const [items, setItems] = useState<GeburtstagLaserTag[]>([]);
+  const [items, setItems] = useState<sondergruppeInterface[]>([]);
   const [addWindow, setAddWindow] = useState(false);
+  const [editWindow, setEditWindow] = useState({ open: false, groupID: 0 });
 
   useEffect(() => {
     if ("caches" in window) {
@@ -22,7 +28,7 @@ function SondergruppenPage() {
     }
   }, []);
 
-  const changeItems = (items: GeburtstagLaserTag[]) => {
+  const changeItems = (items: sondergruppeInterface[]) => {
     setItems(items);
     console.log(items);
 
@@ -30,6 +36,10 @@ function SondergruppenPage() {
       cache.put(`gruppen`, new Response(JSON.stringify(items)));
     });
     setAddWindow(false);
+  };
+
+  const changeEditWindow = (eWindow: typeof editWindow) => {
+    setEditWindow(eWindow);
   };
 
   return (
@@ -48,7 +58,7 @@ function SondergruppenPage() {
         </ol>
       </nav>
       <div className="App Panel">
-        <div className="sonderButtons">
+        <div className="SonderButtons">
           {/* <Link to={``}></Link> */}
           <button
             onClick={() => {
@@ -58,32 +68,43 @@ function SondergruppenPage() {
             Gruppe Hinzufügen
           </button>
         </div>
+
         <br />
-        <h2>Gruppen</h2>
+        <h2>LaserTag</h2>
+        <br />
         <div className="SonderItemList">
           {items.map((i) => {
-            return <ListItem changeItems={changeItems} items={items} item={i} />;
-          })}
-        </div>
-        <br />
-        <h2>Abgeschlossen</h2>
-        <div className="SonderItemList">
-          {items.map((i) => {
-            return <ListItem changeItems={changeItems} items={items} complete item={i} />;
+            return (
+              <>
+                <ListItem
+                  changeEditWindow={changeEditWindow}
+                  changeItems={changeItems}
+                  items={items}
+                  item={i}
+                />
+              </>
+            );
           })}
         </div>
       </div>
       {addWindow && <AddWindow changeItems={changeItems} items={items} />}
+      {editWindow && editWindow.open && items.find((i) => i.id === editWindow.groupID) && (
+        <EditGroupWindow
+          item={items.find((i) => i.id === editWindow.groupID)}
+          changeItems={changeItems}
+          items={items}
+        />
+      )}
     </>
   );
 }
 export default SondergruppenPage;
 
 const AddWindow = (props: {
-  changeItems: (items: GeburtstagLaserTag[]) => void;
-  items: GeburtstagLaserTag[];
+  changeItems: (items: sondergruppeInterface[]) => void;
+  items: sondergruppeInterface[];
 }) => {
-  const [type, setType] = useState<groupsType>();
+  const [type, setType] = useState<bookingTypeInterface>();
   const [players, setPlayers] = useState<number>();
   const [gruppenname, setGruppenname] = useState(`Mustermann`);
   return (
@@ -91,7 +112,7 @@ const AddWindow = (props: {
       <br />
       <div className="App Panel">
         <h2>Gruppe Hinzufügen</h2>
-        <div className="sonderButtons">
+        <div className="SonderButtons">
           <input
             onBlur={(t) => {
               setGruppenname(t.target.value);
@@ -100,16 +121,14 @@ const AddWindow = (props: {
             min={4}
           />
         </div>
-        <div className="sonderButtons">
+        <div className="SonderButtons">
           <select
             onBlur={(t) => {
-              console.log(`oiii`);
-
-              setType(t.target.value as groupsType);
+              setType(bookingTypes[t.target.value]);
             }}
           >
-            {groups.map((s) => {
-              return <option value={s}>{s}</option>;
+            {Object.entries(bookingTypes).map(([key, bookingType]) => {
+              return <option value={key}>{key}</option>;
             })}
           </select>
           <input
@@ -122,55 +141,23 @@ const AddWindow = (props: {
             min={4}
           />
         </div>
-        <div className="sonderButtons">
+        <div className="SonderButtons">
           <button
             onClick={() => {
               if (type && players && gruppenname) {
-                switch (type) {
-                  case `LT Geburtsag x2`:
-                    props.changeItems([
-                      ...props.items,
-                      {
-                        id: getRandomInt(Date.now()),
-                        name: gruppenname,
-                        type: type,
-                        players: players,
-                        softdrinks: [],
-                        softdrinkLimit: players,
-                        sweets: false,
-                      },
-                    ]);
-                    break;
-                  case "LT Geburtsag x3":
-                    props.changeItems([
-                      ...props.items,
-                      {
-                        id: getRandomInt(Date.now()),
-                        name: gruppenname,
-                        type: type,
-                        players: players,
-                        softdrinks: [],
-                        softdrinkLimit: 2 * players,
-                        sweets: false,
-                      },
-                    ]);
-                    break;
-
-                  case "LT Geburtsag x4":
-                    props.changeItems([
-                      ...props.items,
-                      {
-                          id: getRandomInt(Date.now()),
-                        name: gruppenname,
-                        type: type,
-                        players: players,
-                        softdrinks: [],
-                        softdrinkLimit: 3 * players,
-                        sweets: false,
-                      },
-                    ]);
-                    break;
-                }
+                props.changeItems([
+                  ...props.items,
+                  {
+                    id: getRandomInt(Date.now()),
+                    name: gruppenname,
+                    players: players,
+                    beer: [],
+                    nachos: false,
+                    sweets: false,
+                    softdrinks: [],
+                    bookingConfig: type,
+                  },
+                ]);
               }
             }}
           >
@@ -182,39 +169,180 @@ const AddWindow = (props: {
   );
 };
 
+const EditGroupWindow = (props: {
+  changeItems: (items: sondergruppeInterface[]) => void;
+  items: sondergruppeInterface[];
+  item: sondergruppeInterface | undefined;
+}) => {
+  const [type, setType] = useState<bookingTypeInterface>();
+  const [players, setPlayers] = useState<number>();
+  const [gruppenname, setGruppenname] = useState(`Mustermann`);
+  return (
+    <>
+      <br />
+      <div className="App Panel">
+        <h2>{props.item?.name}</h2>
+        <br />
+        <h3>Softgetränke (0,3l)</h3>
+        <div className="CheckListItem">
+          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+          <label htmlFor="vehicle1"> I have a bike</label>
+        </div>
+          <div className="SonderButtons">
+            <select
+              onBlur={(t) => {
+                setType(bookingTypes[t.target.value]);
+              }}
+            >
+              {softdrinks.map((drink) => {
+                return <option value={drink}>{drink}</option>;
+              })}
+            </select>
+            <button
+              onClick={() => {
+                if (type && players && gruppenname) {
+                  props.changeItems([
+                    ...props.items,
+                    {
+                      id: getRandomInt(Date.now()),
+                      name: gruppenname,
+                      players: players,
+                      beer: [],
+                      nachos: false,
+                      sweets: false,
+                      softdrinks: [],
+                      bookingConfig: type,
+                    },
+                  ]);
+                }
+              }}
+            >
+              Hinzufügen
+            </button>
+          </div>
+        <br />
+        <h3>Biere (klein)</h3>
+        <div className="CheckListItem">
+          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+          <label htmlFor="vehicle1"> I have a bike</label>
+        </div>
+          <div className="SonderButtons">
+            <select
+              onBlur={(t) => {
+                setType(bookingTypes[t.target.value]);
+              }}
+            >
+              {softdrinks.map((drink) => {
+                return <option value={drink}>{drink}</option>;
+              })}
+            </select>
+            <button
+              onClick={() => {
+                if (type && players && gruppenname) {
+                  props.changeItems([
+                    ...props.items,
+                    {
+                      id: getRandomInt(Date.now()),
+                      name: gruppenname,
+                      players: players,
+                      beer: [],
+                      nachos: false,
+                      sweets: false,
+                      softdrinks: [],
+                      bookingConfig: type,
+                    },
+                  ]);
+                }
+              }}
+            >
+              Hinzufügen
+            </button>
+          </div>
+        <br />
+      </div>
+    </>
+  );
+};
+
 const ListItem = (props: {
   complete?: boolean;
-  item: GeburtstagLaserTag;
-  items: GeburtstagLaserTag[];
-  changeItems: (items: GeburtstagLaserTag[]) => void;
+  item: sondergruppeInterface;
+  items: sondergruppeInterface[];
+  changeItems: (items: sondergruppeInterface[]) => void;
+  changeEditWindow: (eWindow: { open: boolean; groupID: number }) => void;
 }) => {
   return (
     <>
       <div className={`SonderListItem ${props.complete ? `complete` : ``}`}>
-        <p className="sonderGroupName">{props.item.name}</p>
-        <p className="lasertagName">Kindergeburtstag</p>
+        <p
+          onClick={() => {
+            props.changeEditWindow({ open: true, groupID: props.item.id });
+          }}
+          className="sonderGroupName"
+        >
+          {props.item.name}
+        </p>
+        <p style={{ color: `${props.item.bookingConfig.colorCode}` }} className="typeName">
+          {props.item.bookingConfig.typeName}
+        </p>
         <p className="sonderGroupDetails">
-          <p>
-            Softdrinks: {props.item.softdrinks.length}/{props.item.softdrinkLimit}
-          </p>
-          <p className="SonderSweets"
-            onClick={() => {
-              const elementIndex = props.items.findIndex((i) => i.id === props.item.id);
-              const newItems = [...props.items];
-              newItems[elementIndex] = { ...props.item, sweets: !props.item.sweets };
-              props.changeItems(newItems);
-            }}
-          >
-            Süßigkeiten:{" "}
-            <span style={{color: `${props.item.sweets ? `green` : `red`}`}}>{`${props.item.sweets}`.replace(`false`, `Nicht erhalten`).replace(`true`, `Erhalten`)}</span>
-          </p>
+          {props.item.bookingConfig.softdrinkPerPlayer > 0 && (
+            <p>
+              Softdrinks: {props.item.softdrinks.filter((s) => s.revieved).length} (
+              {props.item.softdrinks.length}) /{" "}
+              {props.item.bookingConfig.softdrinkPerPlayer * props.item.players}
+            </p>
+          )}
+          {props.item.bookingConfig.beerPerPlayer > 0 && (
+            <p>
+              Biere: {props.item.beer.filter((s) => s.revieved).length} ({props.item.beer.length}) /{" "}
+              {props.item.bookingConfig.beerPerPlayer * props.item.players}
+            </p>
+          )}
+
+          {props.item.bookingConfig.sweets && (
+            <p
+              className="SonderSweets"
+              onClick={() => {
+                const elementIndex = props.items.findIndex((i) => i.id === props.item.id);
+                const newItems = [...props.items];
+                newItems[elementIndex] = { ...props.item, sweets: !props.item.sweets };
+                props.changeItems(newItems);
+              }}
+            >
+              Süßigkeiten:{" "}
+              <span style={{ color: `${props.item.sweets ? `green` : `red`}` }}>
+                {`${props.item.sweets}`
+                  .replace(`false`, `Nicht erhalten`)
+                  .replace(`true`, `Erhalten`)}
+              </span>
+            </p>
+          )}
+
+          {props.item.bookingConfig.nachos && (
+            <p
+              className="SonderSweets"
+              onClick={() => {
+                const elementIndex = props.items.findIndex((i) => i.id === props.item.id);
+                const newItems = [...props.items];
+                newItems[elementIndex] = { ...props.item, nachos: !props.item.nachos };
+                props.changeItems(newItems);
+              }}
+            >
+              Nachos:{" "}
+              <span style={{ color: `${props.item.nachos ? `green` : `red`}` }}>
+                {`${props.item.nachos}`
+                  .replace(`false`, `Nicht erhalten`)
+                  .replace(`true`, `Erhalten`)}
+              </span>
+            </p>
+          )}
         </p>
       </div>
     </>
   );
 };
 
-
-function getRandomInt(max:number) {
-    return Math.floor(Math.random() * max);
-  }
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
--- src/SondergruppenTracker/sondergruppenInterfaces.ts
+++ src/SondergruppenTracker/sondergruppenInterfaces.ts
@@ -1,15 +1,62 @@
-export interface GeburtstagLaserTag {
-    id:number;
+export interface sondergruppeInterface {
+  id: number;
   name: string;
-  type:groupsType;
   players: number;
-  softdrinks: softdrinksType[];
-  softdrinkLimit:number;
+  bookingConfig: bookingTypeInterface;
+  softdrinks: { drink: softdrinksType; revieved: boolean }[];
+  beer: { drink: softdrinksType; revieved: boolean }[];
+  nachos: boolean;
   sweets: boolean;
 }
 
-export const groups = [`LT Geburtsag x2`, `LT Geburtsag x3`, `LT Geburtsag x4`] as const;
-export type groupsType = typeof groups[number];
+export const bookingTypes: { [key: string]: bookingTypeInterface } = {
+  "Test": {
+    typeName: "Text Modus",
+    game: "All",
+    colorCode: "#f0f0f0",
+    softdrinkPerPlayer: 2,
+    beerPerPlayer: 1,
+    sweets: true,
+    nachos: true,
+  },
+  "LT Geburtstag x2 (S)": {
+    typeName: "LT Kindergeburtstag x2",
+    game: "Lasertag",
+    colorCode: "#3fbf3f",
+    softdrinkPerPlayer: 1,
+    beerPerPlayer: 0,
+    sweets: true,
+    nachos: false,
+  },
+  "LT Geburtstag x3 (M)": {
+    typeName: "LT Kindergeburtstag x3",
+    game: "Lasertag",
+    colorCode: "#3fbf3f",
+    softdrinkPerPlayer: 2,
+    beerPerPlayer: 0,
+    sweets: true,
+    nachos: false,
+  },
+  "LT Geburtstag x4 (L)": {
+    typeName: "LT Kindergeburtstag x4",
+    colorCode: "#3fbf3f",
+    game: "Lasertag",
+    softdrinkPerPlayer: 3,
+    beerPerPlayer: 0,
+    sweets: true,
+    nachos: false,
+  },
+} as const;
+
+export interface bookingTypeInterface {
+  typeName: string;
+  game: string;
+  colorCode:string;
+  softdrinkPerPlayer: number;
+  beerPerPlayer: number;
+  sweets: boolean;
+  nachos: boolean;
+}
 
 export const softdrinks = [
   `COLA`,
@@ -21,3 +68,5 @@ export const softdrinks = [
   `WASSER-SPRUDEL`,
 ] as const;
 export type softdrinksType = typeof softdrinks[number];
+
+
